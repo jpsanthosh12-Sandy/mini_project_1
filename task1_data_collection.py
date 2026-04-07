d3e29affb810b4602707e6a54538b4e7622ceb5d
jpsanthosh12-Sandy <jpsanthosh12@gmail.com>
2026-04-07 22:22:59 +0530

    Update task1_data_collection.py

--- task1_data_collection.py
+++ task1_data_collection.py
@@ -7,7 +7,7 @@ from datetime import datetime
 # STEP 1: Get top story IDs
 try:
     res = requests.get("https://hacker-news.firebaseio.com/v0/topstories.json")
-    ids = res.json()[:500]
+    ids = res.json()[:50]
     print("Got", len(ids), "IDs")
 except:
     print("Error getting IDs")
